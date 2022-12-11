c15a45835c45bb198073a29d0ceff2112899a1ef
winniehe444 <113746094+winniehe444@users.noreply.github.com>
2022-12-12 03:30:39 +0800

    Add files via upload

--- controller/controllerhome.js
+++ controller/controllerhome.js
@@ -1,1 +1,22 @@
+var Post = require('../model/post');
+var User = require('../model/user');
 
+const controllerhome = {
+    goHome: async(req, res) => {
+        message = '';
+        // if(req.session.username){
+            let posts = await Post.find({}).sort({ 'views': -1 });
+            let latestPosts = await Post.find({}).sort({ 'date': -1 });
+            posts = posts.slice(0, 5);
+            latestPosts = latestPosts.slice(0, 5);
+            res.render('home', { posts, latestPosts });
+        // }
+        // else{
+        //     message = 'Login to proceed.';
+        //     console.log('Login to proceed.');
+        //     res.redirect('/login');
+        // }
+    }
+}
+
+module.exports = controllerhome;
